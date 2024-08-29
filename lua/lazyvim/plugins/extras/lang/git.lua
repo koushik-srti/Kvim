deb33303e37f124dcc4ddf5a9c87f946623f86c7
koushik <koushik.savudam@securiti.ai>
2024-08-29 16:40:31 +0530

    init code

---
+++ lua/lazyvim/plugins/extras/lang/git.lua
@@ -0,0 +1,28 @@
+return {
+  recommended = {
+    ft = { "gitcommit", "gitconfig", "gitrebase", "gitignore", "gitattributes" },
+  },
+  -- Treesitter git support
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "git_config", "gitcommit", "git_rebase", "gitignore", "gitattributes" } },
+  },
+
+  {
+    "nvim-cmp",
+    dependencies = {
+      { "petertriho/cmp-git", opts = {} },
+      {
+        "FabijanZulj/blame.nvim",
+        opts = {
+          blame_options = { "-w" },
+        },
+      },
+    },
+    ---@module 'cmp'
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "git" })
+    end,
+  },
+}
